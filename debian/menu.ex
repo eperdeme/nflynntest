aec1c8874d84c7f659190717d02170f597453629
Nathan Flynn <nathan.flynn@zeninternet.co.uk>
2013-06-18 22:06:57 +0100

    tesicals

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(nflynntest):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="nflynntest" command="/usr/bin/nflynntest"
--- debian/nflynntest.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the nflynntest package
-#
-0 4	* * *	root	[ -x /usr/bin/nflynntest_maintenance ] && /usr/bin/nflynntest_maintenance
